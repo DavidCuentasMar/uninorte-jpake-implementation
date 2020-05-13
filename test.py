ea888a611951c66420c61cdf8e70382d7f05f351
WilliamCadenas21 <williamcadenas21@gmail.com>
2020-05-12 22:37:42 -0500

    flask

---
+++ test.py
@@ -0,0 +1,18 @@
+from jpake import JPAKE
+
+secret2 = "1235"
+secret = "1234"
+alice = JPAKE(secret=secret, signer_id=b"alice")
+bob = JPAKE(secret=secret, signer_id=b"bob")
+
+print(bob.zkp_x1)
+
+# alice.process_one(bob.one())
+# bob.process_one(alice.one())
+
+# alice.process_two(bob.two())
+# bob.process_two(alice.two())
+
+#print(alice.K)
+#print(bob.K)
+#print(eve.k)
