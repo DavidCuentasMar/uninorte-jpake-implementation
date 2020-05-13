3559f1d5ec02bc72ccda5df629bffb1cb148c4ce
WilliamCadenas21 <williamcadenas21@gmail.com>
2020-05-13 16:18:19 -0500

    approach to the first message

--- application.py
+++ application.py
@@ -13,7 +13,17 @@ def hello_world():
 
 @app.route('/test', methods = ['POST'])
 def update_text():
-    print(request.json)
+    
+    #print(request.json)
+    obj = request.json
+    #print(obj)
+    print(obj.zkp_x1)
+    
+    #alice first process
+    # alice.process_one(
+        # remote_gx1=obj['gx1'], remote_zkp_x1=obj['zkp_x1'],
+        # remote_gx2=obj['gx2'], remote_zkp_x2=obj['zkp_x2'],
+    # )
     return {"mensaje":"ok"}
 
 if __name__ == '__main__':
--- clientflask.py
+++ clientflask.py
@@ -8,11 +8,11 @@ url = 'http://127.0.0.1:5000/test'
 secret = "1235"
 bob = JPAKE(secret=secret, signer_id=b"bob")
 
-print(bob.zkp_x1) #how to send this
-
-r = requests.post(url, json={"gr": bob.zkp_x1['gr'], "b":bob.zkp_x1['b'],"id":bob.zkp_x1['id'].decode('utf-8')})
+r = requests.post(url, json={
+        "zpk_x1":{"gr": bob.zkp_x1['gr'], "b":bob.zkp_x1['b'],"id":bob.zkp_x1['id'].decode('utf-8')},
+        "zpk_x2":{"gr": bob.zkp_x2['gr'], "b":bob.zkp_x2['b'],"id":bob.zkp_x2['id'].decode('utf-8')},
+        "gx1": bob.gx1,
+        "gx2": bob.gx2
+    })
 r.status_code
-
-
 print(r.json())
-
