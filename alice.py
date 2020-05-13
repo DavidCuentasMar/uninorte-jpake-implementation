c926c147436c2834c829fd1222bacfe1c08d6573
DavidCuentasMar <davidandrescuentasmar@gmail.com>
2020-05-12 23:28:44 -0500

    setting up communication protocol to jpake config (SOCKETS)

--- alice.py
+++ alice.py
@@ -18,9 +18,18 @@ ADDR = (SERVER, PORT)
 client_custom_id = "1s"
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
-
+jpake_configured = False
 def send(msg):
     message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+def jpake_send(msg):
+    message = msg
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT)
     send_length += b' ' * (HEADER - len(send_length))
@@ -37,8 +46,11 @@ while client_msg != DISCONNECT_MESSAGE:
     secret = "1234"
     alice = JPAKE(secret=secret, signer_id=client_custom_id)
     zpkObj = ZPK(alice.zkp_x1['gr'],alice.zkp_x1['b'],alice.zkp_x1['id'])
-    
+    zpkObj_string = pickle.dumps(zpkObj)
+    if(jpake_configured==False):
+        send('JPAKE_SETTING_UP')
+        jpake_send(zpkObj_string)
+    input()
     #client_msg = input('=> ')
     #send(client_msg)
-    print(pickle.dumps(zpkObj))
 send(DISCONNECT_MESSAGE)
--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket
 import threading
+import pickle
+
 PORT=5050
 HEADER=64
 FORMAT='utf-8'
@@ -10,17 +12,27 @@ ADDR=(SERVER,PORT)
 server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.bind(ADDR)
 
+all_clients = []
+class ZPK:
+    def __init__(self, g,b,id):
+        self.g = g
+        self.b = b
+        self.id = id
+
 def handle_client(conn,addr):
     print(f"[NEW CONNECTION] {addr} connected.")
+    all_clients.append((conn,addr,False))    
     connected = True
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNET_MESSAGE:
-                connected = False
-            print(f"[{addr}] {msg}")
+        for client_obj in all_clients:
+            if client_obj[1]==addr and client_obj[2]==False:
+                if msg_length:
+                    msg_length = int(msg_length)            
+                    msg = conn.recv(msg_length)
+                    data_variable = pickle.loads(msg)          
+                    print(f"[{addr}] {msg}")
+                    print(data_variable)
             conn.send("Msg received".encode(FORMAT))
 
     conn.close()
