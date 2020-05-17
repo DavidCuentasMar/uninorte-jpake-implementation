7cc20361fc9d368c343090ceb0907cf664876e4f
WilliamCadenas21 <williamcadenas21@gmail.com>
2020-05-16 23:17:57 -0500

    start to implement pycrypto

--- clientflask.py
+++ clientflask.py
@@ -1,54 +1,68 @@
 import requests
 import time
 from jpake import JPAKE
- 
+import hashlib 
+from Crypto.Hash import SHA256
 
-url = 'http://127.0.0.1:3000/test'
-url2 = 'http://127.0.0.1:3000/test2'
-url3 = 'http://127.0.0.1:3000/test3'
+aliceId = b'alice'
+
+url = 'http://127.0.0.1:3000/fisrtMessage'
+url2 = 'http://127.0.0.1:3000/secondMessage'
+url3 = 'http://127.0.0.1:3000/onlyForProveTheKey'
 secret = "1235"
 
 
-bob = JPAKE(secret=secret, signer_id=b"bob")
+bob = JPAKE(secret=secret, signer_id="bob")
 
-response = requests.post(url, json={
+try:
+    response = requests.post(url, json={
         "zkp_x1":{"gr": bob.zkp_x1['gr'], "b":bob.zkp_x1['b'],"id":bob.zkp_x1['id'].decode('utf-8')},
         "zkp_x2":{"gr": bob.zkp_x2['gr'], "b":bob.zkp_x2['b'],"id":bob.zkp_x2['id'].decode('utf-8')},
         "gx1": bob.gx1,
         "gx2": bob.gx2
     })
 
-response.status_code
+    response.status_code
 
-#print(response.json())
-data = response.json()
-print(data)
-data['zkp_x1']['id'] = data['zkp_x1']['id'].encode('utf-8')
-data['zkp_x2']['id'] = data['zkp_x2']['id'].encode('utf-8')
-#print(response.json)
-#loaded_json = json.loads(response.json)
-#obj = response.json
-#print(loaded_json)
-#print(obj.zkp_x1)
-#alice first process
-bob.process_one(data)
+    data = response.json()
+    print(data)
+    
+    aliceId = data['zkp_x2']['id']
 
-#print(bob.zkp_A)
-response2 = requests.post(url2, json={
+    data['zkp_x1']['id'] = data['zkp_x1']['id'].encode('utf-8')
+    data['zkp_x2']['id'] = data['zkp_x2']['id'].encode('utf-8')
+
+
+    #bob first process
+    bob.process_one(data)
+except:
+    print('error')
+
+try:
+    response2 = requests.post(url2, json={
     "A":bob.A, 
     "zkp_A":{"gr": bob.zkp_A['gr'], "b":bob.zkp_A['b'],"id":bob.zkp_A['id'].decode('utf-8')}
-})
+    })
+
+    data2 = response2.json()
+    print(data2)
+    data2['zkp_A']['id'] = data2['zkp_A']['id'].encode('utf-8')
+
+    #bob second process
+    bob.process_two(data2)
+
+    print(bob.K)
+    key = bob.k+bob.signer_id+data['zkp_x2']['id']
+except:
+    print('otro error')
 
 
-data2 = response2.json()
-print(data2)
-data2['zkp_A']['id'] = data2['zkp_A']['id'].encode('utf-8')
 
-
-#bob second process
-bob.process_two(data2)
-
-print(bob.K)
+key2 = b'msg'+ bob.signer_id + 
+key = bob.signer_id
+hash = SHA256.new()
+hash.update(key2)
+print(hash.digest())
 
 ##response3 = requests.post(url2, json={"msg": "ey alice todo ok"})
 ##print(response3.json())
--- instructions.md
+++ instructions.md
@@ -1,4 +1,4 @@
-1. implementar AES con Sha
+# 1. implementar AES con Sha
 
 - AES en MODO CBC GSM (mejor modo) cifra y authentica
 - y un sha 256 - keySha = sha(key-j-pake + id_a + id_b)
@@ -8,7 +8,7 @@ keySha = 0000000000000000000000000000000000 | 111111111111111111111111111111111
 k_e = keySha.truncate(128)   0000000000000000000000000000000000 
 k_m = keySha.truncate() 111111111111111111111
 
-2. implentar HMAC
+# 2. implentar HMAC
 
 - H-MAC codigo de authenticacion (con los 128 bits qeu sobraron del trunqueo) -> me regresa C y T
 
@@ -20,15 +20,15 @@ c = texto cifrado con AES
 
 t <------ HMAC(k_m, c)
 
-3. Enviar c y t al servidor (quien en envia)
-
+# 3. Enviar c y t al servidor (quien en envia)
+ 
 Del otro lado (del que recibe)
 
 tomar c y computar t'<----Hmac(k_m, c)
 verificar si t' es igual a t
 
-## otra froma de verlo o como el profesor creyo que va a estar implementado
-V(km, t, m) ---> Fslso o verdadero
+otra froma de verlo o como el profesor creyo que va a estar implementado
+- V(km, t, m) ---> Fslso o verdadero
 
-4. descifrar
+# 4. descifrar
 m<----D_CBC(ke, c)
