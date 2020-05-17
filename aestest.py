88b2ffe0a29f7db924a0c58267c28c86fcaf563a
DavidCuentasMar <davidandrescuentasmar@gmail.com>
2020-05-17 00:30:47 -0400

    exp

--- aestest.py
+++ aestest.py
@@ -4,8 +4,9 @@ from Crypto.Cipher import AES
 key = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
 print ('key', [x for x in key])
 
-iv = ''.join([chr(random.randint(0, 0xFF)) for i in range(16)])
+iv = ' ' * 16
 
+print(len(iv.encode('utf-8')))
 aes = AES.new(key, AES.MODE_CBC, iv)
 data = 'hello world 1234' # <- 16 bytes
 encd = aes.encrypt(data)
