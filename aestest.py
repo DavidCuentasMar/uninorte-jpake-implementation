6e9df73af6d6113f10a143712b7532be48c255db
DavidCuentasMar <davidandrescuentasmar@gmail.com>
2020-05-17 00:18:02 -0400

    example

--- aestest.py
+++ aestest.py
@@ -1,4 +1,5 @@
 import random 
+from Crypto.Cipher import AES
 
 key = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
 print ('key', [x for x in key])
@@ -8,3 +9,4 @@ iv = ''.join([chr(random.randint(0, 0xFF)) for i in range(16)])
 aes = AES.new(key, AES.MODE_CBC, iv)
 data = 'hello world 1234' # <- 16 bytes
 encd = aes.encrypt(data)
+print(encd)
